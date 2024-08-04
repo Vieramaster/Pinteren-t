f41656a41d1f22d0b5b165ac1f2037a36f06fb73
vieramaster <vieramaster@hotmail.com>
2024-08-04 11:22:25 -0300

    hotfix

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -8,39 +8,35 @@ export default function SearchBox({ dataSearch, moreData }) {
       <InfiniteScroll
         pullDownToRefreshThreshold={"250px"}
         className="w-full h-auto flex flex-col items-center"
-        dataLength={dataSearch !== undefined ? dataSearch.length : null}
+        dataLength={dataSearch ? dataSearch.length : null}
         next={moreData ? moreData : null}
         hasMore={true}
         loader={<h4>Loading...</h4>}
       >
         <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5 }} spacing={4}>
-          {dataSearch !== undefined
-            ? dataSearch.map((item) => (
-                <article
-                  key={item.id}
-                  className="w-auto h-auto flex flex-col gap-2 fadeIn"
-                >
-                  <div className="w-auto h-auto ">
-                    <img
-                      src={item.urls.small}
-                      alt={item.alt_description}
-                      className="rounded-3xl"
-                    />
-                  </div>
-                  <p className="font-semibold text-left">
-                    {item.alt_description}
-                  </p>
-                  <div className="flex  gap-2 items-center">
-                    <img
-                      src={item.user.profile_image.small}
-                      alt="user photo"
-                      className="rounded-full"
-                    />
-                    <h3>{item.user.username}</h3>
-                  </div>
-                </article>
-              ))
-            : null}
+          {dataSearch?.map((item) => (
+            <article
+              key={item.id}
+              className="w-auto h-auto flex flex-col gap-2 fadeIn"
+            >
+              <div className="w-auto h-auto ">
+                <img
+                  src={item.urls.small}
+                  alt={item.alt_description}
+                  className="rounded-3xl"
+                />
+              </div>
+              <p className="font-semibold text-left">{item.alt_description}</p>
+              <div className="flex  gap-2 items-center">
+                <img
+                  src={item.user.profile_image.small}
+                  alt="user photo"
+                  className="rounded-full"
+                />
+                <h3>{item.user.username}</h3>
+              </div>
+            </article>
+          ))}
         </Masonry>
       </InfiniteScroll>
     </section>
