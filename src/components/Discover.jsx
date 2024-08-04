0b8b32d864645c25b364df27bbb8001d8a16629e
vieramaster <vieramaster@hotmail.com>
2024-08-04 11:14:57 -0300

    add hovers

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -30,27 +30,25 @@ export default function Discover({ setSearchValue }) {
       </h1>
 
       <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5 }} spacing={4}>
-        {dataDiscover !== undefined
-          ? dataDiscover.map((item, index) => {
-              return (
-                <button
-                  onClick={() => setSearchValue(item.slug)}
-                  key={index}
-                  className="relative rounded-xl overflow-auto w-auto h-auto flex flex-col gap-2 fadeIn"
-                >
-                  <span className="absolute inset-0 bg-black bg-opacity-50"></span>
-                  <img
-                    src={item.cover_photo.urls.thumb}
-                    alt={item.cover_photo.alt_description}
-                    className="h-full w-full"
-                  />
-                  <h3 className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-semibold text-2xl">
-                    {item.slug}
-                  </h3>
-                </button>
-              );
-            })
-          : null}
+        {dataDiscover?.map((item, index) => {
+          return (
+            <button
+              onClick={() => setSearchValue(item.slug)}
+              key={index}
+              className=" relative rounded-xl overflow-hidden w-auto h-auto flex flex-col gap-2 fadeIn group "
+            >
+              <span className="absolute inset-0 bg-black bg-opacity-50 z-30"></span>
+              <img
+                src={item.cover_photo.urls.thumb}
+                alt={item.cover_photo.alt_description}
+                className="h-full w-full group-hover:scale-105 duration-200 ease-in-out"
+              />
+              <h3 className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-semibold text-2xl z-50">
+                {item.slug}
+              </h3>
+            </button>
+          );
+        })}
       </Masonry>
     </section>
   );
