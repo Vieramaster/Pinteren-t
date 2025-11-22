06cbe921a89fe6bd105123afa9bd01e31c88d1a6
vieramaster <vieramaster@hotmail.com>
2025-11-21 22:31:41 -0300

    reorganize the project folders

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
-import "./index.css";
+import "../styles/index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
